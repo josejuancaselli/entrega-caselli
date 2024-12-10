a63550e1bfe5dd85c570418fbbe1718201ed5d92
josecaselli <josejuancaselli@gmail.com>
2024-12-10 02:47:34 -0300

    estilos

--- src/components/itemlist/Item.jsx
+++ src/components/itemlist/Item.jsx
@@ -4,12 +4,14 @@ import './itemlist.css';
 
 const Item = ({ product }) => {
     return (
-        <div className="card h-100 text-center">
-            <Link to={`/item/${product.id}`} className="text-decoration-none text-dark">
-                <img src={product.img} alt={product.nombre} className="card-img-top" />
-                <div className="card-body">
-                    <h5 className="card-title">{product.nombre} {product.descripcion}</h5>
-                    <p className="card-text">${product.precio.toLocaleString('es-AR')}</p>
+        <div className="tarjeta ">
+            <Link to={`/item/${product.id}`} className="text-decoration-none">
+                <img src={product.img} alt={product.nombre} className="tarjeta-img" />
+                <div className="info">
+                    <h3>{product.nombre}</h3>
+                    <p>{product.descripcion}</p>
+                    <p>${product.precio.toLocaleString('es-AR')}</p>
+                    <button className="tarjeta-button">Ver más</button>
                 </div>
             </Link>
         </div>
